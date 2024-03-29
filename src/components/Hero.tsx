59a9251be71412e01d76934ddceda55d8944a035
shoaib khan <shoaib.khan.880066@gmail.com>
2024-03-30 02:25:55 +0530

    Auto-commit: Changes made on save

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,11 @@ import hero from "../assets/hero.png";
 const Hero = () => {
   return (
     <div>
-      <img src={hero} alt="hero_image" />
+      <img
+        src={hero}
+        alt="hero_image"
+        className="w-full max-h-[600px] object-cover"
+      />
     </div>
   );
 };
